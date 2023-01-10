8f77a112f7f04daaa21345d1fbdd505368311a5a
hanabens <94543855+hanabens@users.noreply.github.com>
2023-01-10 11:45:01 +0100

    etape_3

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -1,19 +1,16 @@
 #include <Arduino.h>
 
-
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 #define SENSOR 33
 #define LED 26
 
-#define DHTTYPE    DHT11   
+#define DHTTYPE DHT11
 DHT_Unified dht(SENSOR, DHTTYPE);
 
-
-
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   pinMode(LED, OUTPUT);
   Serial.begin(9600);
@@ -23,56 +20,81 @@ void setup() {
   dht.temperature().getSensor(&sensor);
   Serial.println(F("------------------------------------"));
   Serial.println(F("Temperature Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("°C"));
   Serial.println(F("------------------------------------"));
   // Print humidity sensor details.
   dht.humidity().getSensor(&sensor);
   Serial.println(F("Humidity Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("%"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("%"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("%"));
   Serial.println(F("------------------------------------"));
   // Set delay between sensor readings based on sensor details.
 
+  //
+  //
+  //
 
-}
-
-void loop() {
-  
   // Effectuer une mesure toutes les 5s
-   delay(5000);
+  // delay(5000);
 
-     // Afficher la température avec un peu de formatage
-     
-    sensors_event_t event;
+  // Afficher la température avec un peu de formatage
+
+  sensors_event_t event;
   dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
+  if (isnan(event.temperature))
+  {
     Serial.println(F("Error reading temperature!"));
   }
-  else {
+  else
+  {
     Serial.print(F("Temperature: "));
     Serial.print(event.temperature);
     Serial.println(F("°C"));
   }
 
   // Afficher l'humidité relative avec un peu de formatage
-     dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity))
+  {
     Serial.println(F("Error reading humidity!"));
   }
-  else {
+  else
+  {
     Serial.print(F("Humidity: "));
     Serial.print(event.relative_humidity);
     Serial.println(F("%"));
   }
 
-
+  // Deep sleep -> optimisation de la batterie en micro seconde
+  esp_sleep_enable_timer_wakeup(5000000);
+  esp_deep_sleep_start();
 }
+
+void loop() {}
